c9533d3c2dc81c1e5e392bde9560d913123268be
qiluge <921444844@qq.com>
2018-07-23 14:52:25 +0800

    add concent

---
+++ main.go
@@ -0,0 +1,215 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/axgle/mahonia"
+	"github.com/ontio/ontology/common/log"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+	"strings"
+	"sync"
+)
+
+type item struct {
+	OriginalLink string
+	NewPostion   string
+}
+
+type DocMap []item
+
+const DOC_REP_PATH = "../../ontio/documentation/"
+
+var linkMap = make(map[string]string, 0)
+
+func main() {
+	log.InitLog(log.InfoLog, log.Stdout, log.PATH)
+
+	// read doc map
+	docMapFileContent, err := ioutil.ReadFile("doc-map.json")
+	if err != nil {
+		log.Errorf("read doc map err: %s \n!", err)
+		return
+	}
+	var docMap DocMap
+	err = json.Unmarshal(docMapFileContent, &docMap)
+	if err != nil {
+		log.Errorf("unmarshal doc map err: %s \n!", err)
+		return
+	}
+	// read link map
+	linkMapFileContent, err := ioutil.ReadFile("link-map.json")
+	if err != nil {
+		log.Errorf("read link map err: %s \n!", err)
+		return
+	}
+	err = json.Unmarshal(linkMapFileContent, &linkMap)
+	if err != nil {
+		log.Errorf("unmarshal link map err: %s \n!", err)
+		return
+	}
+	var wg = new(sync.WaitGroup)
+	for _, v := range docMap {
+		url := v.OriginalLink
+		if !strings.Contains(url, "http") {
+			continue
+		}
+		filePath := DOC_REP_PATH + v.NewPostion
+		wg.Add(1)
+		go handleFile(url, filePath, wg)
+	}
+	wg.Wait()
+	jsonBytes, _ := json.Marshal(&linkMap)
+	jsonString := string(jsonBytes)
+	jsonString = strings.Replace(jsonString, ":\"", ":\n\"", -1)
+	jsonString = strings.Replace(jsonString, ",", ",\n", -1)
+	ioutil.WriteFile("link-map.json", []byte(jsonString), 0644)
+}
+
+func handleFile(url, filePath string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	originContent, err := download(url)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	log.Infof("download success: url is %s, destination is %s.", url, filePath)
+	newContent := handleContent(string(originContent), url, filePath)
+	log.Info("handle content success:", filePath)
+	err = writeToNewPosition([]byte(newContent), filePath)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	log.Info("write to new position success:", filePath)
+}
+
+func download(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	defer func() {
+		if resp != nil && resp.Body != nil {
+			resp.Body.Close()
+		}
+	}()
+	if err != nil {
+		err = fmt.Errorf("download file connection err: %s, url is %s!\n", err, url)
+		return []byte{}, err
+	}
+	if err != nil || resp.StatusCode != 200 {
+		err = fmt.Errorf("download file err: %s, url is %s, response is %s!\n", err, url, resp.Status)
+		return []byte{}, err
+	}
+	utf8Decoder := mahonia.NewDecoder("utf-8")
+	decoderReader := utf8Decoder.NewReader(resp.Body)
+	originContent, err := ioutil.ReadAll(decoderReader)
+	if err != nil {
+		err = fmt.Errorf("read respong body err: %s", err)
+		return []byte{}, err
+	}
+	return originContent, nil
+}
+
+func handleContent(originContent, downloadUrl, filePath string) string {
+
+	// get info from file path
+	filePath = strings.Replace(filePath, DOC_REP_PATH, "", 1)
+	splitPath := strings.Split(filePath, "/")
+	firstDir := splitPath[2]  // doc_zh, doc_en
+	secondDir := splitPath[3] // Ontology, DID, Dapp, Introduction, SDKs
+	fileName := splitPath[4]
+
+	// replace relative link
+	linkMapKeyPrefix := strings.Replace(downloadUrl, "https://raw.githubusercontent.com/ontio/", "", 1)
+	linkMapKeyPrefix = strings.Replace(linkMapKeyPrefix, "master/", "", 1)
+	newContent := replaceLink(originContent, linkMapKeyPrefix)
+
+	// handle title and version info
+	newContent = handleTitleAndVersion(newContent)
+
+	headerUrl := strings.Replace(downloadUrl, "raw.githubusercontent", "github", 1)
+	headerUrl = strings.Replace(headerUrl, "/master/", "/blob/master/", 1)
+	header := constructHeader(headerUrl, firstDir, secondDir, fileName)
+	newContent = header + newContent
+	return newContent
+}
+
+func replaceLink(originContent, prefix string) string {
+	// remove godoc, go report card, travis, gitter link
+	goDocLinkReg := regexp.MustCompile(`\[!.*\]\(.*\)`)
+	goDocLinkRegReuslt := goDocLinkReg.FindAllString(originContent, -1)
+	for _, v := range goDocLinkRegReuslt {
+		originContent = strings.Replace(originContent, v, "", -1)
+	}
+	// extract link(relative path)
+	linkReg := regexp.MustCompile(`\[.*?\]\([^#].*?[^html]\)`)
+	result := linkReg.FindAllString(originContent, -1)
+
+	for _, extractLink := range result {
+		if strings.Contains(extractLink, "http") || strings.Contains(extractLink, "html") {
+			continue
+		}
+		// [xxxx](aaa.md)
+		linkMapKey := prefix + extractLink
+		if newLink, ok := linkMap[linkMapKey]; ok {
+			// construct new link
+			leftIndex := strings.Index(extractLink, "(")
+			newPositionLink := extractLink[:leftIndex+1] + newLink + ")"
+			originContent = strings.Replace(originContent, extractLink, newPositionLink, 1)
+		} else {
+			linkMap[linkMapKey] = ""
+		}
+	}
+	return originContent
+}
+
+func handleTitleAndVersion(originContent string) string {
+
+	versionReg := regexp.MustCompile(`(?m:^.*[v|V]ersion\s\d\.\d\.\d.*$)`)
+	version := versionReg.FindAllString(originContent, 1)
+	var versionInfo string
+	if len(version) == 0 {
+		versionInfo = "<p align=\"center\" class=\"version\">Version 1.0.0 </p>"
+	}
+
+	titleReg := regexp.MustCompile(`(?m:^#\s.*$)`)
+	title := titleReg.FindAllString(originContent, 1)
+	if len(title) == 1 {
+		titleInfo := "<h1 align=\"center\">" + title[0][2:] + "</h1>"
+		replaceString := titleInfo + "\n\n" + versionInfo + "\n\n"
+		originContent = strings.Replace(originContent, title[0], replaceString, 1)
+	}
+	return originContent
+}
+
+func constructHeader(url, firstDir, secondDir, fileName string) string {
+	var sidebar string
+	if strings.Contains(firstDir, "zh") {
+		sidebar = secondDir + "_zh"
+	} else {
+		sidebar = secondDir + "_en"
+	}
+	permalink := strings.Replace(fileName, ".md", ".html", 1)
+	folder := firstDir + "/" + secondDir
+	// add header
+	header := "---\n"
+	header += "title:\n"
+	header += "keywords: sample homepage\n"
+	header += "sidebar: " + sidebar + "\n"
+	header += "permalink: " + permalink + "\n"
+	header += "folder: " + folder + "\n"
+	header += "giturl: " + url + "\n"
+	header += "---\n\n"
+	return header
+}
+
+func writeToNewPosition(fileContent []byte, filePath string) error {
+
+	err := ioutil.WriteFile(filePath, fileContent, 0644)
+	if err != nil {
+		return fmt.Errorf("writeToNewPosition, write to file %s err: %s, content length is %d",
+			filePath, err, len(fileContent))
+	}
+	return nil
+}
---
+++ reg_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"regexp"
+	"strings"
+	"testing"
+)
+
+func TestLinkReplace(t *testing.T) {
+	fileContent, _ := ioutil.ReadFile("testnet.md")
+	//fileContent:=[]byte("English | [中文](testnet_CN.md)")
+	linkReg := regexp.MustCompile(`\[.*?\]\([^#].*.*?\)`)
+	result := linkReg.FindAll(fileContent, -1)
+	fmt.Printf("%q\n", result)
+}
+
+func TestEncoder(t *testing.T) {
+	name := "1234百万红包"
+	words := ([]rune)(name)
+	fmt.Printf("Before :%s\n", string(words))
+	afterWords := strings.Replace(string(words), string([]rune("百万")), string([]rune("两个")), -1)
+	fmt.Printf("After :%s\n", string(afterWords))
+}
+
+func TestDownload(t *testing.T) {
+	url := "https://raw.githubusercontent.com/ontio/ontology-DID/master/docs/cn/ONTID_protocol_spec_cn.md"
+	result, _ := download(url)
+	ioutil.WriteFile("test.md", result, 0644)
+}
